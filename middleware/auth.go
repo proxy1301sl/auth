680036231b5e664fdf721eebcb2ce4e050a4ed38
dfgfdg <dfgfdgf@luvscars.local>
2026-09-13 03:26:48 +0300

    add middleware for get req

--- middleware/auth.go
+++ middleware/auth.go
@@ -4,16 +4,33 @@ import (
 	"context"
 	"net/http"
 	"strings"
+
+	"https/github.com/proxy1301sl/auth/token"
 )
 
-func AuthMiddleware(next http.Handler, ctx context.Context) http.Handler {
+type ContextKey string
+
+const UserIdKey ContextKey = "userId"
+
+func AuthMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		req := r.Header.Get("Authorization")
-		_, token, ok := strings.Cut(req, "Bearer ")
-		if !ok {
+		tokenf := strings.TrimPrefix(req, "Bearer ")
+		if tokenf == "" {
 			w.WriteHeader(http.StatusUnauthorized)
 			return
 		}
-
+		if tokenf == "" || tokenf != req {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+		claims, err := token.VerifyJWT(tokenf)
+		if err != nil {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+		ctx := context.WithValue(r.Context(), UserIdKey, claims.Subject)
+		r = r.WithContext(ctx)
+		next.ServeHTTP(w, r.WithContext(ctx))
 	})
 }
